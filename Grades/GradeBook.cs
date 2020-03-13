b704910ff7d7326248069e96c9812b0749fbd820
Elev <Elev@DESKTOP-GEF6JMJ>
2020-03-13 16:29:11 +0100

    6.3

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -12,9 +12,9 @@ namespace Grades
         public GradeStatistics ComputeStatistics()
         {
             GradeStatistics stats = new GradeStatistics();
-            
+
             float sum = 0;
-            foreach(float grade in grades)
+            foreach (float grade in grades)
             {
                 stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
                 stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
@@ -28,7 +28,21 @@ namespace Grades
         {
             grades.Add(grade);
         }
-        public string Name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                { 
+                    
+                }           
+            }
+        }
+        private string _name;
         private List<float> grades;
     }
 }
